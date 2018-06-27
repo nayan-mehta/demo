945cc3dd7f146ffb4a33cba85b94deacc2584cfe
Nayan Mehta <nayan.taurian@gmail.com>
2018-06-27 22:33:23 +0530

    Race car added text,blocks and songs

---
+++ racecar_game.py
@@ -0,0 +1,109 @@
+# Added text for 'You crashed'
+# Added things right to display at different ranges and for them to move
+# Added songs
+
+import pygame
+import time
+import random
+
+pygame.init()
+#Convert mp3 to .wav and enjoy songs yay!! :D
+
+pygame.mixer.music.load('bouncey.wav')
+pygame.mixer.music.play(-1)
+
+display_width = 800
+display_height = 600
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('A bit Racey')
+carImg = pygame.image.load('racecar.png')
+
+black = (0,0,0)
+white = (255,255,255)
+
+clock = pygame.time.Clock()
+
+racecar_width=73
+
+def things(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf',115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+    pygame.display.update()
+    # time.sleep(2)
+    game_loop()
+
+def crash():
+    message_display('You Crashed')
+
+def car(x,y):
+    gameDisplay.blit(carImg, (x,y))
+
+def game_loop():
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
+    x_change = 0
+    y_change = 0
+    gameExit = False
+    thing_startx = random.randrange(0, display_width)
+    thing_starty = -600
+    thing_speed = 7
+    thing_width = 100
+    thing_height = 100
+    while not gameExit:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                # gameExit = True
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+                elif event.key == pygame.K_UP:
+                    y_change = -5
+                elif event.key == pygame.K_DOWN:
+                    y_change = 5
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    x_change = 0
+                    y_change = 0
+
+            # print(event)
+        x += x_change
+        y += y_change
+
+        gameDisplay.fill(white)
+
+        things(thing_startx, thing_starty, thing_width, thing_height, black)
+        thing_starty += thing_speed
+
+        car(x, y)
+        if((x < 0) or (x > display_width - racecar_width)):
+            # gameExit = True
+             crash()
+        if(thing_starty > display_height):
+            thing_starty = -600
+            thing_startx = random.randrange(0,display_width)
+
+
+
+        pygame.display.update()
+
+        clock.tick(60)
+
+game_loop()
+pygame.quit()
+quit()
+
